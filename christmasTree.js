373ef667f8fd67604e86b905fb2c94b5a7e05cad
Yuliia Pasichnyk <yuliia.pasichnyk@delightex.com>
2024-04-21 16:38:45 +0200

    HW - Print Triangle (add UI interface)

--- christmasTree.js
+++
@@ -1,9 +0,0 @@
-function printTree(count = 5, symbol = '*') {
-  let row = symbol; 
-  console.log(row);
-  for (let i = 1; i < count; i++) {
-    row = row + symbol;
-    console.log(row);
-  }
-}
-printTree(5, '&')
