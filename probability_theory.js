eb7d6980c05890fe4d5a3258f39a6db57bf4a914
Yuliia Pasichnyk <yuliia.pasichnyk@delightex.com>
2024-04-23 22:48:38 +0200

    HW - refactoring code

--- probability_theory.js
+++ probability_theory.js
@@ -1,29 +1,18 @@
+function checkProbabilityTheory(count) {
+    let even = 0
+    let odd = 0
 
-let count = 5
-let minValue = 100
-let maxValue = 1000
-
-let even = 0
-let odd = 0
-let persent = 0
-
-for (let i = 1; i <= count; i++) {
-    let number = getRandomInt(minValue, maxValue)
-    if (number % 2 === 0) {
-        even++
-    } else {
-        odd++
+    for (let i = 1; i <= count; i++) {
+        let number = Math.floor(Math.random() * (1000 - 100) + 100)
+        if (number % 2 === 0) {
+            even++
+        } else {
+            odd++
+        }
     }
-
+    let percent = (even / count) * 100;
+    return `count = ${count}, even = ${even}, odd = ${odd}, percentage = ${Math.floor(percent)}%`
 }
 
-function getRandomInt(min, max) {
-    min = Math.ceil(min);
-    max = Math.floor(max);
-    return Math.floor(Math.random() * (max - min) + min); 
-}
-
-let percentage = (even / count) * 100;
-
-console.log(`even = ${even}, odd = ${odd}, percentage = ${Math.floor(percentage)}%`);
+console.log(checkProbabilityTheory(5));
 
