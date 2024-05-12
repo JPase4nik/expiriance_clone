37311ed84f44711859b66a82c6f3f6d1c7f0f235
Yuliia Pasichnyk <yuliia.pasichnyk@delightex.com>
2024-05-12 15:37:51 +0200

    HW10 - Paranoya

---
+++ paranoya.js
@@ -0,0 +1,33 @@
+const infos = [
+    {
+        userName:"Test",
+        lastName:"Test",
+        email:"test.test@gmail.com>"
+    },
+    {
+        userName:"Dmitro",
+        lastName:"Porohov",
+        email:"dmitro.porohov@yahoo.com>"
+    },
+    {
+        userName:"Andrii",
+        lastName:"",
+        email:"andrii@mail.ru>" // Нам такі не підходять
+    },
+];
+
+const pattern = /@(gmail|yahoo)\.com/g; // створюємо регулярний вираз
+
+const result = []
+for (const info of infos) {  // перебираєм масив об'єктів
+    pattern.lastIndex = 0;
+    if (pattern.test(info.email)) {  // перевіряєм чи містить рядок патерн
+        result.push(info)
+    }
+}
+
+console.table(result);
+
+
+
+
